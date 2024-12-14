5be5fd581819c7aef3e25089e39aecc8b225c90b
Kathryn Carbone <katncarbon@gmail.com>
2024-12-14 19:45:09 +0000

    Addressing comments

--- v1/src/generate_sam_masks.py
+++ v1/src/generate_sam_masks.py
@@ -1,16 +1,20 @@
 """
 A really quick script for generating SAM masks for v1 dataset images.
+
+TODO(carbonkat): Move the segment anything stuff + this script
+into its own folder.
 """
 
 import os
 
-from numpy import load
+import numpy as np
 import numpy as np
 from PIL import Image
 from segment_anything import sam_model_registry
 from segment_anything import SamPredictor
 from skimage.measure import regionprops
 from tqdm import tqdm
+import data.medgeese_v1_utils as utils
 
 
 # TODO(carbonkat): Link this to the function in medgeese_v1_utils.py which does
@@ -45,13 +49,13 @@ def expand_3d(
 def main():
 
     # TODO(carbonkat): link this with the datadir parameter set in the paths file
-    # in configs.
-    data_dir = "<insert data directory here>"
-    mask_dir = "<insert desired mask directory here>"
+    # in configs (possibly use https://click.palletsprojects.com/en/stable/).
+    data_dir = "/home/carbok/MedGeese/v1/src/data/dataset_files/v1/ground_truths"
+    mask_dir = "/home/carbok/MedGeese/v1/src/data/dataset_files/v1/masks"
 
     # TODO(carbonkat): make this path dynamic
     sam = sam_model_registry["default"](
-        checkpoint="<insert checkpoint file path here>"
+        checkpoint="/home/carbok/MedGeese/v1/src/segment_anything/sam_vit_h_4b8939.pth"
     )
     sam.to(device="cuda")
     mask_predictor = SamPredictor(sam)
@@ -59,58 +63,60 @@ def main():
     file_folder = []
     if not os.path.exists(mask_dir):
         os.mkdir(mask_dir)
-
+    
+    # This walks through the data directory structure, retrieves
+    # the desired files for processing, gets their parent folders
+    # (modality folder, i.e. XRay and subfolder i.e. specific dataset
+    # they originally belonged to), and makes a mask directory
+    # with this same structure to put the new data into.
     for root, _, files in os.walk(data_dir):
-        for file in files:
+        if mask_dir in root:
+            continue
+        else:
+            for file in files:
+                if file.endswith(".npz"):
+                    original_path = root
+                    modality_subfolder = original_path.replace(data_dir, "")[1:]
+                    modality_folder = os.path.dirname(modality_subfolder)
 
-            # TODO(carbonkat): make this much more elegant
-            if file.endswith(".npz") and mask_dir not in root:
-                original_path = root
-                modality_subfolder = original_path.replace(data_dir, "")[1:]
-                modality_folder = os.path.dirname(modality_subfolder)
+                    new_mask_folder = os.path.join(mask_dir, modality_folder)
+                    new_mask_subfolder = os.path.join(mask_dir, modality_subfolder)
+                    # Make new mask directories if they do not already exist
+                    if not os.path.exists(new_mask_folder):
+                        os.mkdir(new_mask_folder)
+                    if not os.path.exists(new_mask_subfolder):
+                        os.mkdir(new_mask_subfolder)
 
-                new_mask_folder = os.path.join(mask_dir, modality_folder)
-                new_mask_subfolder = os.path.join(mask_dir, modality_subfolder)
-                # Make new mask directories if they do not already exist
-                if not os.path.exists(new_mask_folder):
-                    os.mkdir(new_mask_folder)
-                if not os.path.exists(new_mask_subfolder):
-                    os.mkdir(new_mask_subfolder)
-
-                full_path = os.path.join(root, file)
-                # Skip over files if they have already been generated
-                if os.path.exists(os.path.join(new_mask_subfolder, file)):
-                    continue
-                else:
+                    full_path = os.path.join(root, file)
+                    # Skip over files if they have already been generated
+                    #if os.path.exists(os.path.join(new_mask_subfolder, file)):
+                    #    continue
+                    #else:
                     file_folder.append(full_path)
-
+  
     for f in tqdm(file_folder):
         new_folder = os.path.dirname(f).replace(data_dir, "")[1:]
         mask_folder = os.path.join(mask_dir, new_folder)
 
-        packed_data = load(f)
-        try:
-            img = packed_data["imgs"]
-            mask = packed_data["gts"]
-        except Exception as e:
-            print(packed_data.keys())
-            print(e)
+        packed_data = np.load(f)
+        img = packed_data["imgs"]
+        mask = packed_data["gts"]
 
         if len(np.unique(mask)) == 1:
             continue
 
         data_dict = {"imgs": img}
-
+        
         # If a 3D volume, get image and mask slices for
         # each timestep.
         if len(img.shape) > 2 and img.shape[2] != 3:
-            imgs, masks = expand_3d(img, mask)
+            imgs, masks = utils.expand_2d(img, mask)
         else:
             imgs = [img]
             masks = [mask]
 
         all_masks = []
-        for i, candidate_img in enumerate(imgs):
+        for mask, candidate_img in zip(imgs, masks):
             # SAM expects RBG images, so need to convert to correct
             # format.
             if len(candidate_img.shape) < 3:
@@ -121,7 +127,7 @@ def main():
             mask_predictor.set_image(candidate_img)
             # Compute bounding boxes for each segmentation artifact
             # in the original mask.
-            bboxes = regionprops(masks[i])
+            bboxes = regionprops(mask)
             new_masks = []
             for prop in bboxes:
                 bbox = prop.bbox
@@ -140,7 +146,7 @@ def main():
                 m = SAM_masks.squeeze()
                 new_masks.append(m)
 
-            blank_canvas = np.zeros_like(masks[i])
+            blank_canvas = np.zeros_like(mask)
 
             # Iterate through each mask and gradually add to
             # the full reconstructed mask. NOTE: this assumes
@@ -149,11 +155,8 @@ def main():
                 blank_canvas[m != 0] = 255
             all_masks.append(blank_canvas)
 
-        if len(all_masks) > 1:
-            final_mask = np.asarray(all_masks)
-        else:
-            final_mask = all_masks[0]
-
+        final_mask = np.asarray(all_masks).squeeze()
+        assert final_mask.shape == mask.shape
         data_dict["gts"] = final_mask
         np.savez(
             os.path.join(mask_folder, os.path.basename(f)),
