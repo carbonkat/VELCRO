8ac801b6726f967606a04ef99ee8d83dd4ab1b55
Kathryn Carbone <katncarbon@gmail.com>
2024-12-17 15:22:08 +0000

    final fixes

--- v1/src/generate_sam_masks.py
+++ v1/src/generate_sam_masks.py
@@ -8,7 +8,6 @@ into its own folder.
 import os
 
 import numpy as np
-import numpy as np
 from PIL import Image
 from segment_anything import sam_model_registry
 from segment_anything import SamPredictor
@@ -17,53 +16,22 @@ from tqdm import tqdm
 import data.medgeese_v1_utils as utils
 
 
-# TODO(carbonkat): Link this to the function in medgeese_v1_utils.py which does
-# the same thing. I think this will require treating the dataset_files.v1 folder
-# as a package.
-def expand_3d(
-    image: np.array, mask: np.array
-) -> tuple[list[np.array], list[np.array]]:
-    """
-    Function for expanding 3D volumes and extracting nonzero-ed masks.
-    Expects 3D volumes to be expanded along axis 0.
-
-    Args:
-        image (np.array): numpy array of original 3D volume
-        mask (np.array): numpy array of 3D volume masks
-
-    Returns:
-        tuple[list[np.array], list[np.array]]: paired list of expanded
-        images and masks
-    """
-
-    images, masks = [], []
-    for i in range(mask.shape[0]):
-        if len(np.unique(mask[i])) == 1:
-            continue
-        else:
-            images.append(image[i])
-            masks.append(mask[i])
-    return (images, masks)
-
-
 def main():
 
     # TODO(carbonkat): link this with the datadir parameter set in the paths file
     # in configs (possibly use https://click.palletsprojects.com/en/stable/).
-    data_dir = "/home/carbok/MedGeese/v1/src/data/dataset_files/v1/ground_truths"
-    mask_dir = "/home/carbok/MedGeese/v1/src/data/dataset_files/v1/masks"
+    data_dir = "<data_dir here>"
+    mask_dir = "<mask_dir here>"
 
     # TODO(carbonkat): make this path dynamic
-    sam = sam_model_registry["default"](
-        checkpoint="/home/carbok/MedGeese/v1/src/segment_anything/sam_vit_h_4b8939.pth"
-    )
+    sam = sam_model_registry["default"](checkpoint="<sam_checkpoint_path here>")
     sam.to(device="cuda")
     mask_predictor = SamPredictor(sam)
 
     file_folder = []
     if not os.path.exists(mask_dir):
         os.mkdir(mask_dir)
-    
+
     # This walks through the data directory structure, retrieves
     # the desired files for processing, gets their parent folders
     # (modality folder, i.e. XRay and subfolder i.e. specific dataset
@@ -77,23 +45,15 @@ def main():
                 if file.endswith(".npz"):
                     original_path = root
                     modality_subfolder = original_path.replace(data_dir, "")[1:]
-                    modality_folder = os.path.dirname(modality_subfolder)
-
-                    new_mask_folder = os.path.join(mask_dir, modality_folder)
-                    new_mask_subfolder = os.path.join(mask_dir, modality_subfolder)
+                    new_mask_subfolder = os.path.join(
+                        mask_dir, modality_subfolder
+                    )
                     # Make new mask directories if they do not already exist
-                    if not os.path.exists(new_mask_folder):
-                        os.mkdir(new_mask_folder)
-                    if not os.path.exists(new_mask_subfolder):
-                        os.mkdir(new_mask_subfolder)
+                    os.makedirs(new_mask_subfolder, exist_ok=True)
 
                     full_path = os.path.join(root, file)
-                    # Skip over files if they have already been generated
-                    #if os.path.exists(os.path.join(new_mask_subfolder, file)):
-                    #    continue
-                    #else:
                     file_folder.append(full_path)
-  
+
     for f in tqdm(file_folder):
         new_folder = os.path.dirname(f).replace(data_dir, "")[1:]
         mask_folder = os.path.join(mask_dir, new_folder)
@@ -106,11 +66,11 @@ def main():
             continue
 
         data_dict = {"imgs": img}
-        
+
         # If a 3D volume, get image and mask slices for
         # each timestep.
         if len(img.shape) > 2 and img.shape[2] != 3:
-            imgs, masks = utils.expand_2d(img, mask)
+            imgs, masks = utils.extract_2d_masks(img, mask)
         else:
             imgs = [img]
             masks = [mask]
